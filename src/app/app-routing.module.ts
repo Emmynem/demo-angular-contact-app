088f3989f9562cef77654215a6929cb7dd591ec6
Emmanuel Nwoye <41800908+Emmynem@users.noreply.github.com>
2022-01-31 23:04:46 +0100

    Removed routes that are unnecessary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { NameEditorComponent } from './name-editor/name-editor.component';
-import { SignInComponent } from './sign-in/sign-in.component';
 import { NotFoundComponent } from './404/404.component';
 
 
@@ -13,8 +11,6 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'profile-editor', component: ProfileEditorComponent },
   { path: 'edit-profile/:id', component: EditProfileComponent },
-  { path: 'name-editor', component: NameEditorComponent },
-  { path: 'sign-in', component: SignInComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
